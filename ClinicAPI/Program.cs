7a1b7d75a8ac50e8007f200fae7856a9396d5f73
Ali Alawi <mralooyg@gmail.com>
2026-04-28 16:38:08 +0300

    Remove broken migration

--- ClinicAPI/Program.cs
+++ ClinicAPI/Program.cs
@@ -7,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<ClinicDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+           .ConfigureWarnings(w => w.Ignore(Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.PendingModelChangesWarning)));
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
 {
